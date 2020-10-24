fbb98dc017962259ecd75b3ff472d73166a2ffa7
localdisk <smoochynet@gmail.com>
2020-10-24 14:32:18 +0900

    add image upload.

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('content')
 <div class="w-3/4 mx-auto pt-10">
+  <h2 class="text-2xl mt-4 mb-1">Example</h2>
   <livewire:example />
+  <h2 class="text-2xl mt-4 mb-1">FileUpload</h2>
+  <livewire:file-upload />
 </div>
 @endsection
